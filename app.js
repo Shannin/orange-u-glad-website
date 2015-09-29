a6cc19e37e185f4972f5a73b8fc19ac88cf3927d
Shannin Ciprich <shanninc@gmail.com>
2015-09-28 18:21:07 -0700

    add Mailchimp API wrapper, add to list boiler, random css cleanups

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var MailChimpAPI = require('mailchimp').MailChimpAPI;
+
+var apiKey = '';
+var mailchimp = MailChimpAPI(apiKey, { version : '2.0' });
 
 var assetDir = 'public';
 var port = 8080;
 
 app.use(express.static(assetDir));
 
-app.get('/api/hello', function(req, res) {
-    console.log('Hello World');
-    res.send('hello');
+app.get('/api/addEmailToList', function(req, res) {
+    var params = {
+        id: '',
+        batch: [{
+            email: {
+                email: 'shanninc@gmail.com',
+            },
+            email_type: 'html',
+        }],
+        double_optin: false, // dont send opt in email
+    }
+
+    mailchimp.call('lists', 'batch-subscribe', params, function (error, data) {
+        console.log(error);
+        console.log(data);
+
+        res.send('added');
+    });
 });
 
 // viewed at http://localhost:8080
