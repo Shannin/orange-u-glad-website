4ccf106b940935d20c24723317d3a6eb697228db
Shannin Ciprich <shanninc@gmail.com>
2015-12-03 18:31:59 -0800

    use jquery to keep things in line testimonials

--- js/components/testimonials.js
+++ js/components/testimonials.js
@@ -1,17 +1,38 @@
 (function($) {
     "use strict"; // Start of use strict
 
-    var testimonials = $("h3", ".testimonials__testimonial-wrapper");
-    var idx = -1;
-    var transition = 3500;
+    var wrapper = $(".testimonials__testimonial-wrapper");
+    var testimonials = $("h3", wrapper);
+    var showingIndex = -1;
+
+    function resetTestimonialPositions() {
+        var maxHeight = 0;
+
+        testimonials.each(function (idx, t) {
+            var height = $(t).height();
+            if (height > maxHeight) {
+                maxHeight = $(t).height()
+            }
+        });
+
+        testimonials.each(function (idx, t) {
+            var top = (maxHeight - $(t).height()) / 2;
+            $(t).css('padding-top', top);
+        });
+
+        wrapper.height(maxHeight);
+    }
 
     function cycle() {
-        idx++;
-        testimonials.eq(idx % testimonials.length)
+        showingIndex++;
+        testimonials.eq(showingIndex % testimonials.length)
             .fadeIn(2000)
             .delay(3500)
             .fadeOut(2000, cycle);
     }
-    
+
+    resetTestimonialPositions();
     cycle();
+
+    $(window).on('resize', resetTestimonialPositions);
 })(jQuery);
