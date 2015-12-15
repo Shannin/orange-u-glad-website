77389d9f9449792e38abab8b98fa2f5c13fb7d31
Shannin Ciprich <shanninc@gmail.com>
2015-12-15 10:22:59 -0800

    lots of cleanupsg

--- js/main.js
+++ js/main.js
@@ -3,11 +3,12 @@
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
         event.preventDefault();
+
+        var $anchor = $(this);
+        $(document.body).animate({
+            'scrollTop':   $($anchor.attr('href')).offset().top
+        }, 1250);
     });
 
     function adjustPage() {
