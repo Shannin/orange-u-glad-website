4d98b68bd0337fbc018cb43032ac3f18824b7f65
Shannin Ciprich <shanninc@gmail.com>
2016-01-20 22:45:16 -0800

    labs: make form work

--- js/labs.js
+++ js/labs.js
@@ -1,8 +1,19 @@
 var labs = {
-
-}
+    'A0201': [],
+};
 
 (function($) {
     "use strict"; // Start of use strict
+
+    $("#lab-results__form").submit(function (event) {
+        console.log("called")
+
+        event.preventDefault();
+        var form = $(this);
+        var lotNumber = $('input[name="lot-number"]', form);
+
+        console.log(lotNumber);
+
+    });
 
 })(jQuery);
