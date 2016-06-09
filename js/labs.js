7894a557a21af63f7310ec509c89ec2e5776b244
Shannin Ciprich <shanninc@gmail.com>
2016-06-09 11:13:20 -0700

    add new lab

--- js/labs.js
+++ js/labs.js
@@ -3,7 +3,7 @@
 
     var labs = {
         'A0201': 'http://www.cwcannalytical.com/cwlims/verify?cwid=CW7214S01&share=1',
-        'OUGO214': 'http://www.cwcannalytical.com/cwlims/verify?cwid=CW9190S01&share=1',
+        'OUG214': 'http://www.cwcannalytical.com/cwlims/verify?cwid=CW9190S01&share=1',
     };
 
     $("#lab-results__form").submit(function (event) {
